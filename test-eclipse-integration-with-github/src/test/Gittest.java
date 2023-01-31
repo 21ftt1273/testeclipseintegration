e347e79a74d23b99a5d0eb689cc140365229fedb
Amirul Aiman <21FTT1273@student.pb.edu.bn>
2023-01-31 15:29:24 +0800

    Second commit from Eclipse, still nervous.

--- test-eclipse-integration-with-github/src/test/Gittest.java
+++ test-eclipse-integration-with-github/src/test/Gittest.java
@@ -3,5 +3,7 @@ package test;
 public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("Amiana Grande was here.");
+		ClassA classA = new ClassA("ABCDEF");
+		System.out.println(classA.getAbc());
 	}
 }
