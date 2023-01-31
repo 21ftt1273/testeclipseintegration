5d1329bcf943eafcd2335b54b545f0e5a1a50ce6
Wan Wafiy Hasif <21FTT1278@student.pb.edu.bn>
2023-01-31 15:53:54 +0800

    put your ussy in it

--- test-eclipse-integration-with-github/src/test/Gittest.java
+++ test-eclipse-integration-with-github/src/test/Gittest.java
@@ -3,7 +3,7 @@ package test;
 public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("Amiana Grande was here.");
-		ClassA classA = new ClassA("ABCDEF");
+		ClassA classA = new ClassA("MIRUSSY");
 		System.out.println(classA.getAbc());
 	}
 }
